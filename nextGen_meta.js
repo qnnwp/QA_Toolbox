dba8ffaac8c080036b963835fd86d854192d8cee
cirept <cirept612@gmail.com>
2017-03-02 12:45:36 -0800

    v3.2 edit
    meta script
    - did not include @include

--- nextGen_meta.js
+++ nextGen_meta.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name NextGen Migration Toolbar
+// @name NextGen QA Toolbar
 // @namespace www.cobaltgroup.com/
 // @version 3.2
 // @author Eric Tanaka
+// @include http:*
 // @description NextGen Migration Toolbar
-// @downloadURL https://cdn.rawgit.com/cirept/NextGen/master/nextGen_meta.js
+// @downloadURL https://rawgit.com/cirept/NextGen/master/nextGen_meta.js
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @require https://use.fontawesome.com/3953f47d82.js
 // @require https://cdn.rawgit.com/cirept/NextGen/v3.2/nextGen_ToolBox.js
