13f88a6806a49992670f592a4d46566e07018ce0
Eric Tanaka <eric.tanaka>
2017-05-26 09:58:54 -0700

    Updated meta tag to new branch URL for testing.

--- nextGen_meta.js
+++ nextGen_meta.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name QA Tool
 // @namespace www.cobaltgroup.com/
-// @version 3.2.7
+// @version 3.2.8
 // @author Eric Tanaka
 // @include http:*
 // @include https:*
 // @description QA Toolbar
-// @downloadURL https://rawgit.com/cirept/NextGen/master/nextGen_meta.js
+// @downloadURL https://raw.githubusercontent.com/cirept/NextGen/QLP-%26-NextGen-Support/nextGen_meta.js
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @require https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
-// @require https://cdn.rawgit.com/cirept/NextGen/v3.2.7/nextGen_ToolBox.js
+// @require https://raw.githubusercontent.com/cirept/NextGen/QLP-%26-NextGen-Support/nextGen_ToolBox.js
 // @resource fontAwe https://use.fontawesome.com/3953f47d82.js
 // @resource font https://fonts.googleapis.com/css?family=Montserrat
 // @resource jqueryUI https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css
