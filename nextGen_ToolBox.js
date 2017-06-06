54154d3df430cceb23e4ef93491e5e3620e26809
Eric Tanaka <eric.tanaka>
2017-06-06 12:36:00 -0700

    moving as much css styles out of javascript code.

--- nextGen_ToolBox.js
+++ nextGen_ToolBox.js
@@ -1795,7 +1795,8 @@
             // tier 2 functions
             // ----------------------------------------
             addStyles: function () {
-                this.$toolbarStyles.append('.spell-check.misspelled { display: inline-block !important; vertical-align: initial !important; padding: 0; margin: 0; color: inherit !important; font: inherit; background-color: rgb(255, 182, 193); background-position: bottom; background-repeat: repeat-x; background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAMAAACDKl70AAAAFVBMVEX////+NQD+NQD+NQD+Sgz+NQD+NQCKU3Z/AAAAB3RSTlMAKVdwqL/ggPdRNgAAABdJREFUCNdjYGRiZGBgYWVhYGBmY2YAAADPAB54rWlqAAAAAElFTkSuQmCC")}');
+                this.$toolbarStyles
+                    .append('.spell-check.misspelled { display: inline-block !important; vertical-align: initial !important; padding: 0; margin: 0; color: inherit !important; font: inherit; background-color: rgb(255, 182, 193); background-position: bottom; background-repeat: repeat-x; background-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAADCAMAAACDKl70AAAAFVBMVEX////+NQD+NQD+NQD+Sgz+NQD+NQCKU3Z/AAAAB3RSTlMAKVdwqL/ggPdRNgAAABdJREFUCNdjYGRiZGBgYWVhYGBmY2YAAADPAB54rWlqAAAAAElFTkSuQmCC")}');
             },
             buildLegendContent: function () {
                 var $contentArray = spellCheck.config.$legendContent,
