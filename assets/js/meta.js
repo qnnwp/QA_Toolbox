28c4473952a0c7fe8d42bf57a9afde2e67e68d86
Eric Tanaka <eric.tanaka>
2017-06-29 13:31:45 -0700

    removed description text from meta

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -5,7 +5,6 @@
 // @author Eric Tanaka
 // @include http:*
 // @include https:*
-// @description Support to nextgen increased.  Added modified functionality of link checker, image checker, and 404 link checker.
 // @downloadURL https://raw.githubusercontent.com/cirept/NextGen/separateCss/nextGen_meta.js
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @require https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
