c66f24fc4fe722aa816c7790074e2cbd245b2bfa
Eric Tanaka <eric.tanaka>
2017-07-07 13:57:04 -0700

    updated meta tag to make room for newest release

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -1,19 +1,19 @@
 // ==UserScript==
 // @name QA Toolbox
 // @namespace www.cobaltgroup.com/
-// @version 4.0.0
+// @version 3.3.0
 // @author Eric Tanaka
 // @include http:*
 // @include https:*
-// @downloadURL https://raw.githubusercontent.com/cirept/NextGen/separateCss/nextGen_meta.js
+// @downloadURL https://raw.githubusercontent.com/cirept/QA_Toolbox/master/meta.js
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @require https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
-// @require https://raw.githubusercontent.com/cirept/NextGen/separateCss/nextGen_ToolBox.js
+// @require https://raw.githubusercontent.com/cirept/QA_Toolbox/v3.3.0/toolBox.js
 // @resource fontAwe https://use.fontawesome.com/3953f47d82.js
 // @resource font https://fonts.googleapis.com/css?family=Montserrat
 // @resource jqueryUI https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css
 // @resource typo https://raw.githubusercontent.com/cirept/Typo.js/master/typo/typo.js
-// @resource toolStyles https://raw.githubusercontent.com/cirept/NextGen/separateCss/assets/css/toolbox.css
+// @resource toolStyles https://raw.githubusercontent.com/cirept/QA_Toolbox/separateCss/assets/css/toolbox.css
 // @resource animate https://raw.githubusercontent.com/cirept/animate.css/master/animate.css
 // @resource localToolStyles file:///C:\Users\tanakae\Documents\GitHub\QA_Toolbox\assets\css\toolbox.css
 // @run-at document-end
