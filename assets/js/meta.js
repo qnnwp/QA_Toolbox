c636750263435d41aa14f1daf2cd51410f3ef919
Eric Tanaka <eric.tanaka>
2017-07-12 09:18:20 -0700

    added code to inject toolbox css.

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -8,9 +8,10 @@
 // @downloadURL https://raw.githubusercontent.com/cirept/QA_Toolbox/master/meta.js
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @require https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
+// @require https://use.fontawesome.com/3953f47d82.js
+// @require https://raw.githubusercontent.com/cirept/Typo.js/master/typo/typo.js
 // @require https://raw.githubusercontent.com/cirept/QA_Toolbox/3.3.1-beta-6/assets/js/toolBox.js
-// @required https://use.fontawesome.com/3953f47d82.js
-// @required https://raw.githubusercontent.com/cirept/Typo.js/master/typo/typo.js
+// @resource toolboxStyles https://raw.githubusercontent.com/cirept/QA_Toolbox/3.3.1-beta-6/assets/css/toolbox.css
 // @run-at document-end
 // @grant GM_openInTab
 // @grant GM_setClipboard
@@ -19,8 +20,11 @@
 // @grant GM_getValue
 // @grant GM_info
 // @grant GM_listValues
+// @grant GM_addStyle
+// @grant GM_getResourceText
+// @grant resource
 // @noframes
 // ==/UserScript==
 
-// link to changelog
-// https://github.com/cirept/NextGen/blob/master/guides/CHANGELOG.md
+var cusCSS = GM_getResourceText("toolboxStyles");
+GM_addStyle(cusCSS);
