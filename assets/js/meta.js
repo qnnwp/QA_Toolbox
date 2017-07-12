ac275724fdbbc13e55b372a0299dfac600cde293
Eric Tanaka <eric.tanaka>
2017-07-12 08:39:14 -0700

    updated to 3.3.1

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name QA Toolbox
 // @namespace www.cobaltgroup.com/
-// @version 3.3.0
+// @version 3.3.1
 // @author Eric Tanaka
 // @include http:*
 // @include https:*
 // @downloadURL https://raw.githubusercontent.com/cirept/QA_Toolbox/master/meta.js
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @require https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
-// @require https://raw.githubusercontent.com/cirept/QA_Toolbox/v3.3.0/toolBox.js
+// @require https://raw.githubusercontent.com/cirept/QA_Toolbox/v3.3.1/toolBox.js
 // @required https://use.fontawesome.com/3953f47d82.js
 // @required https://raw.githubusercontent.com/cirept/Typo.js/master/typo/typo.js
 // @run-at document-end
