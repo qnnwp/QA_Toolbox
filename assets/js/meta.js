73226eb51f233b531b6982f20278f4b919bf158b
Eric Tanaka <cirept612@gmail.com>
2017-07-17 18:23:41 -0700

    moved css and some of the js resources into the toolBox.js file

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -1,17 +1,14 @@
 // ==UserScript==
-// @name QA Toolbox v3.3.1.1
+// @name QA Toolbox v3.3.1.2-testing
 // @namespace www.cobaltgroup.com/
-// @version 3.3.1.1
+// @version 3.3.1.2-testing
 // @author Eric Tanaka
 // @include http:*
 // @include https:*
-// @downloadURL https://raw.githubusercontent.com/cirept/QA_Toolbox/master/assets/js/meta.js
+// @downloadURL https://rawgit.com/cirept/QA_Toolbox/master/assets/js/meta.js
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @require https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
-// @require https://use.fontawesome.com/3953f47d82.js
-// @require https://raw.githubusercontent.com/cirept/Typo.js/master/typo/typo.js
-// @require https://raw.githubusercontent.com/cirept/QA_Toolbox/3.3.1.1/assets/js/toolBox.js
-// @resource toolboxStyles https://raw.githubusercontent.com/cirept/QA_Toolbox/3.3.1.1/assets/css/toolbox.css
+// @require https://rawgit.com/cirept/QA_Toolbox/3.3.1.2-testing/assets/js/toolBox.js
 // @run-at document-end
 // @grant GM_openInTab
 // @grant GM_setClipboard
@@ -25,7 +22,3 @@
 // @grant resource
 // @noframes
 // ==/UserScript==
-
-/* eslint new-cap: 0, no-undef: 0 */
-var cusCSS = GM_getResourceText('toolboxStyles');
-GM_addStyle(cusCSS);
