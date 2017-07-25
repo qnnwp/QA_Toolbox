ac3347ed2f7d897421ab35551ebc7b66662eaba3
Eric Tanaka <eric.tanaka>
2017-07-25 12:06:58 -0700

    updated css file url

--- assets/js/toolBox.js
+++ assets/js/toolBox.js
@@ -254,8 +254,7 @@
                 }),
                 '$toolStyles': jQuery('<link>').attr({
                     'id': 'toolStyles',
-                    'href': 'https://rawgit.com/cirept/QA_Toolbox/' + shared.branchName() + '/assets/css/toolbox.css',
-                    //                                                        'href': 'https://rawgit.com/cirept/QA_Toolbox/' + GM_info.script.version + '/assets/css/toolbox.css', // eslint-disable-line new-cap
+                    'href': 'https://rawgit.com/cirept/QA_Toolbox/' + GM_info.script.version + '/assets/css/toolbox.css', // eslint-disable-line new-cap
                     'rel': 'stylesheet',
                     'type': 'text/css',
                 }),
