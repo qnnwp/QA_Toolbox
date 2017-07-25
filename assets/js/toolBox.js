30e15ed1c0a39092f17670b1e75078eeec9f2c16
Eric Tanaka <eric.tanaka>
2017-07-25 12:44:57 -0700

    new fork to remove cache of toolbox file on rawgit servers

--- assets/js/toolBox.js
+++ assets/js/toolBox.js
@@ -259,7 +259,7 @@
                 }),
                 '$toolStyles': jQuery('<link>').attr({
                     'id': 'toolStyles',
-                    'href': 'https://rawgit.com/cirept/QA_Toolbox/testOutNewMetaTag/assets/css/toolbox.css', // eslint-disable-line new-cap
+                    'href': 'https://rawgit.com/cirept/QA_Toolbox/newMetaTag/assets/css/toolbox.css', // eslint-disable-line new-cap
                     //                    'href': 'https://rawgit.com/cirept/QA_Toolbox/' + GM_info.script.version + '/assets/css/toolbox.css', // eslint-disable-line new-cap
                     'rel': 'stylesheet',
                     'type': 'text/css',
