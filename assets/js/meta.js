fa1ec471f98aa04e0a356e1175423df68c98f749
Eric Tanaka <eric.tanaka>
2017-07-25 12:12:23 -0700

    change file URL in meta tag to lead directly to github

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -5,8 +5,8 @@
 // @author Eric Tanaka
 // @include http:*
 // @include https:*
-// @downloadURL https://rawgit.com/cirept/QA_Toolbox/testOutNewMetaTag/assets/js/meta.js
-// @require https://rawgit.com/cirept/QA_Toolbox/testOutNewMetaTag/assets/js/toolBox.js
+// @downloadURL https://raw.githubusercontent.com/cirept/QA_Toolbox/testOutNewMetaTag/assets/js/meta.js
+// @require https://raw.githubusercontent.com/cirept/QA_Toolbox/testOutNewMetaTag/assets/js/toolBox.js
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @require https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
 // @run-at document-end
