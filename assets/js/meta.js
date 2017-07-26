d8bcd76d85ca91fac71733377bbaad12cd5dc413
Eric Tanaka <eric.tanaka>
2017-07-26 08:24:28 -0700

    getting ready to merge with master

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -1,15 +1,17 @@
 // ==UserScript==
-// @name QA Toolbox 3.3.1.2-NewMeta
+// @name QA Toolbox v3.3.1.1
 // @namespace www.cobaltgroup.com/
-// @version 3.3.1.2-NewMeta
+// @version 3.3.1.1
 // @author Eric Tanaka
 // @include http:*
 // @include https:*
-// @downloadURL https://rawgit.com/cirept/QA_Toolbox/3.3.1.2-NewMeta/assets/js/meta.js
+// @downloadURL https://raw.githubusercontent.com/cirept/QA_Toolbox/master/assets/js/meta.js
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @require https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
-// @require https://cdn.rawgit.com/cirept/Typo.js/master/typo/typo.js
-// @require https://rawgit.com/cirept/QA_Toolbox/3.3.1.2-NewMeta/assets/js/toolBox.js
+// @require https://use.fontawesome.com/3953f47d82.js
+// @require https://raw.githubusercontent.com/cirept/Typo.js/master/typo/typo.js
+// @require https://raw.githubusercontent.com/cirept/QA_Toolbox/3.3.1.1/assets/js/toolBox.js
+// @resource toolboxStyles https://raw.githubusercontent.com/cirept/QA_Toolbox/3.3.1.1/assets/css/toolbox.css
 // @run-at document-end
 // @grant GM_openInTab
 // @grant GM_setClipboard
@@ -19,6 +21,10 @@
 // @grant GM_info
 // @grant GM_listValues
 // @grant GM_addStyle
+// @grant GM_getResourceText
 // @grant resource
 // @noframes
 // ==/UserScript==
+
+var cusCSS = GM_getResourceText('toolboxStyles');
+GM_addStyle(cusCSS);
