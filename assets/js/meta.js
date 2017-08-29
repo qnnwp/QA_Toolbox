b9aea79073bc06b620f6e9c8ce981fe07f23e887
Eric Tanaka <eric.tanaka>
2017-08-28 17:06:25 -0700

    fixed the spell checker issue, v3.3.1.3
    - revised spell checker code.
    - added function comments
    - added typo.js call to meta tag

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name QA Toolbox 3.3.1.2
+// @name QA Toolbox 3.3.1.3
 // @namespace www.cobaltgroup.com/
-// @version 3.3.1.2
+// @version 3.3.1.3
 // @author Eric Tanaka
 // @include http:*
 // @include https:*
@@ -9,7 +9,7 @@
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @require https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
 // @require https://cdn.rawgit.com/cirept/Typo.js/master/typo/typo.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.1.2/assets/js/toolBox.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.1.3/assets/js/toolBox.js
 // @run-at document-end
 // @grant GM_openInTab
 // @grant GM_setClipboard
