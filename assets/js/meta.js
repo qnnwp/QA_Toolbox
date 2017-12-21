7855f54a842e9452192b665bce42f4cd71fd0a2a
Eric Tanaka <cirept612@gmail.com>
2017-12-20 21:52:20 -0800

    updated meta code
    updated file path to @required js file

--- assets/js/meta.js
+++ assets/js/meta.js
@@ -9,7 +9,7 @@
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @require https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js
 // @require https://cdn.rawgit.com/cirept/Typo.js/master/typo/typo.js
-// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.1.4/assets/js/toolBox.js
+// @require https://cdn.rawgit.com/cirept/QA_Toolbox/3.3.1.4-prerelease/assets/js/toolBox.js
 // @run-at document-end
 // @grant GM_openInTab
 // @grant GM_setClipboard
